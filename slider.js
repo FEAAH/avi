b7d7a9041318819ce51ed7f6ecb405b41c7437ca
Henrik Svensson <you@example.com>
2022-04-20 11:15:35 +0200

    initialised

---
+++ slider.js
@@ -0,0 +1,29 @@
+const prev = document.querySelector("#back").addEventListener("click", skift);
+const next = document.querySelector("#next").addEventListener("click", skift);
+const slides = document.querySelectorAll(".slide");
+const caption = document.querySelector(".caption");
+
+let current = 0;
+let captionText = slides[current].getAttribute("alt");
+caption.textContent = captionText;
+
+function skift() {
+
+    slides[current].classList.remove("slide--active", "left", "right");
+    if (this.getAttribute("id") == "next") {
+        current++;
+        if (current === slides.length) {
+            current = 0;
+        }
+        slides[current].classList.add("slide--active", "left");
+    } else {
+        current--;
+        if (current < 0) {
+            current = (slides.length - 1);
+        }
+        slides[current].classList.add("slide--active", "right");
+    }
+    captionText = slides[current].getAttribute("alt");
+    caption.textContent = captionText;
+
+}
